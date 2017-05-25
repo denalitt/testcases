43f39b82e3c6c8dcf2d5ba4969639a7245d52a6c
root <root@denaliA-ubuntu0.juniper.net>
2017-05-25 01:57:29 -0400

    sszabo: adding test cases

---
+++ op/jsnapy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# /usr/local/bin/jsnapy --snapcheck pre -f cw_lab_snapcheck.yml
+# cd /root/tests; /usr/local/bin/jsnapy --snapcheck pre -f lab_snapcheck.yml
+cd /root/tests; /usr/local/bin/jsnapy --snapcheck pre -f cfg_r.yml
+
+
+
+
+
+
